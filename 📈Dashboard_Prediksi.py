f81fbe3ae4a7569fd3ef5d1185d7e7c66b47c16f
Maulaya Radhibilla <abilradhibilla@gmail.com>
2024-04-01 11:09:37 +0700

    add new metrics harga rata2 prediksi

--- 📈Dashboard_Prediksi.py
+++ 📈Dashboard_Prediksi.py
@@ -7,6 +7,8 @@ import altair as alt
 from pytorch_forecasting import TemporalFusionTransformer
 from functools import reduce
 import datetime
+import locale
+import locale;locale.setlocale(locale.LC_ALL, 'id_ID.UTF-8')
 import warnings
 warnings.filterwarnings("ignore")
 
@@ -42,11 +44,11 @@ st.sidebar.markdown('# [Prediksi Model](#3)')
 
 st.title('Prediksi Harga Pangan')  
 
-st.subheader('Prediksi', divider='blue', anchor = '3')
-
 max_prediction_length = 30
 max_encoder_length = 60
+
 with st.form("prediksi"):
+    st.subheader('Set parameter', divider='blue', anchor = '3')
     latest_data = data['Tanggal'].max()
     pilihan_komoditas_prediksi = st.selectbox(
         "Pilih Jenis",
@@ -54,7 +56,7 @@ with st.form("prediksi"):
         placeholder="Pilih",
         )
     tanggal_awal_prediksi = st.date_input("Tanggal Awal prediksi", value = latest_data + datetime.timedelta(days=1))
-    pred_button = st.form_submit_button("Submit")
+    pred_button = st.form_submit_button("Run prediction")
 
 if pred_button:
     encoder, extended_df = mf.create_outofsample_base(data, df_merged, tanggal_awal_prediksi, max_encoder_length, max_prediction_length)
@@ -68,4 +70,15 @@ if pred_button:
     df_prediction = mf.filter_prediction(raw_result, output_dict, pilihan_komoditas_prediksi, pred_date_index)
 
     alt_predchart = mf.create_chart_pred(df_prediction)
+
+    st.markdown('#### Grafik')
     st.altair_chart((alt_predchart).interactive(), use_container_width=True)
+
+    st.markdown('##### Matriks')
+
+    filtered_data = data[data['jenis'] == pilihan_komoditas_prediksi]
+    mean_last_30 = round(filtered_data.tail(30)['harga'].mean())
+    mean_pred = round(df_prediction['Harga Prediksi'].mean())
+    
+    percentage_difference = (round(((mean_pred - mean_last_30) / mean_last_30) * 100,1))
+    st.metric('Rata-rata harga prediksi', locale.currency(mean_pred, grouping=True)[:-3], f'{percentage_difference}%')
