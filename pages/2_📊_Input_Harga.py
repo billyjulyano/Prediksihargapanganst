b50a5a35f72369c63f3efdf2be697a920e5f6cb9
Maulaya Radhibilla <abilradhibilla@gmail.com>
2024-04-02 13:41:10 +0700

    i dont know

--- pages/2_📊_Input_Harga.py
+++ pages/2_📊_Input_Harga.py
@@ -1,7 +1,7 @@
 import streamlit as st
-
+loginstate = False
 def login(username, password):
-    if username == 'admin' and password == 'abc123':
+    if username == 'admin' and password == '1':
         return True
     else:
         return False
@@ -15,8 +15,10 @@ def main():
     if st.button('Login'):
         if login(username, password):
             st.success('Login successful!')
+            st.switch_page("your_app.py")
         else:
             st.error('Invalid username or password')
 
 if __name__ == '__main__':
     main()
+
