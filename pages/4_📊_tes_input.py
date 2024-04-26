4364cff05b960df4350c63c4143be881ea476270
Maulaya Radhibilla <abilradhibilla@gmail.com>
2024-04-26 18:40:29 +0700

    form for add new row data:done

--- pages/4_📊_tes_input.py
+++ pages/4_📊_tes_input.py
@@ -1,82 +1,51 @@
 from common import *
 
-st.write('# Solution using a dataframe')
+st.write('# Add new data')
 
 # Create an empty dataframe on first page load, will skip on page reloads
-if 'data' not in st.session_state:
-    data = pd.DataFrame({'Tanggal': [],
+if 'updated_data' not in st.session_state:
+    updated_data = pd.DataFrame({'Tanggal': [],
                         'ProduksiBeras': [],
                         'occasion': [],
                         'StokCipinang': [],
                         'Kurs': [],
                         'BerasPremium': [],
                         'BerasMedium': []})
-    st.session_state.data = data
+    st.session_state.updated_data = updated_data
 
 # Show current data
-st.dataframe(st.session_state.data)
+st.dataframe(st.session_state.updated_data, use_container_width = True)
 
-st.write('#### Using form submission')
+st.write('#### Use form below:')
 
 # Function to append inputs from form into dataframe
 def add_dfForm():
-    row = pd.DataFrame({'Tanggal':[st.session_state.input_df_form_date],
+    st.session_state.input_df_form_occasion = '-'
+    date_input = st.session_state.input_df_form_date
+    date_input = pd.to_datetime(date_input)
+    row = pd.DataFrame({'Tanggal':[date_input],
                         'ProduksiBeras':[st.session_state.input_df_form_ProduksiBeras],
                         'occasion':[st.session_state.input_df_form_occasion],
                         'StokCipinang':[st.session_state.input_df_form_StokCipinang],
                         'Kurs':[st.session_state.input_df_form_kurs],
                         'BerasPremium': [st.session_state.input_df_form_BP],
                         'BerasMedium':[st.session_state.input_df_form_BM]})
-    st.session_state.data = pd.concat([st.session_state.data, row])
+    st.session_state.updated_data = pd.concat([st.session_state.updated_data, row])
 
 # Inputs listed within a form
-# dfForm = st.form(key='dfForm', clear_on_submit=True)
-# with dfForm:
-#     dfFormColumns = st.columns(6)
-#     with dfFormColumns[0]:
-#         st.date_input('tanggal', key='input_df_form_date')
-#         date_input = st.session_state.input_df_form_date
-#         date_input = pd.to_datetime(date_input)
-#     with dfFormColumns[1]:
-#         st.number_input('Produksi Beras (ton)', step=1, key='input_df_form_ProduksiBeras')
-#         ProduksiBeras = st.session_state.input_df_form_ProduksiBeras
-#     with dfFormColumns[2]:
-#         st.number_input('Stok Cipinang (ton)', step=1, key='input_df_form_StokCipinang')
-#         StokCipinang = st.session_state.input_df_form_StokCipinang
-#     with dfFormColumns[3]:
-#         st.number_input('Kurs', step=1, key='input_df_form_kurs')
-#         kurs_input = st.session_state.input_df_form_kurs
-#     with dfFormColumns[4]:
-#         st.number_input('Beras Premium', step=1, key='input_df_form_BP')
-#         BP_input = st.session_state.input_df_form_BP
-#     with dfFormColumns[5]:
-#         st.number_input('Beras Medium', step=1, key='input_df_form_BM')
-#         BM_input = st.session_state.input_df_form_BM
-#     st.session_state.input_df_form_occasion = '-'
-#     occasion_input = st.session_state.input_df_form_occasion
-#     st.form_submit_button(on_click=add_dfForm)
-
-dfForm = st.form(key='dfForm', clear_on_submit=True)
+dfForm = st.form(key='dfForm', clear_on_submit=False)
 with dfForm:
     dfFormColumns = st.columns(6)
     with dfFormColumns[0]:
-        date_input = st.date_input('tanggal', key='input_df_form_datex')
-        date_input = pd.to_datetime(date_input)
-        st.session_state.input_df_form_date = date_input
+        st.date_input('tanggal', key='input_df_form_date')
     with dfFormColumns[1]:
-        ProduksiBeras = st.number_input('Produksi Beras (ton)', step=1, key='input_df_form_ProduxksiBeras')
-        st.session_state.input_df_form_ProduksiBeras = ProduksiBeras
+        st.number_input('Produksi Beras', step=13401, key='input_df_form_ProduksiBeras', value = 1200327)
     with dfFormColumns[2]:
-        StokCipinang = st.number_input('Stok Cipinang (ton)', step=1, key='input_df_form_StokCxipinang')
-        st.session_state.input_df_form_StokCipinang = StokCipinang
+        st.number_input('Stok Cipinang', step=897, key='input_df_form_StokCipinang', value = 31030)
     with dfFormColumns[3]:
-        kurs_input = st.number_input('Kurs', step=1, key='input_df_xform_kurs')
-        st.session_state.input_df_form_kurs = kurs_input
+        st.number_input('Kurs', step=457, key='input_df_form_kurs', value = 12300)
     with dfFormColumns[4]:
-        BP_input = st.number_input('Beras Premium', step=1, key='input_xdf_form_BP')
-        st.session_state.input_df_form_BP = BP_input
+        st.number_input('Beras Premium', step=343, key='input_df_form_BP', value = 16991)
     with dfFormColumns[5]:
-        BM_input = st.number_input('Beras Medium', step=1, key='input_dfx_form_BM')
-        st.session_state.input_df_form_BM = BM_input
-    st.session_state.input_df_form_occasion = '-'
+        st.number_input('Beras Medium', step=242, key='input_df_form_BM', value = 13661)
     st.form_submit_button(on_click=add_dfForm)
